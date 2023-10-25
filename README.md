58dbdb1680a0c56095293ed851a8b14769231a4c
ubinaroy <roychen37326@outlook.com>
2023-10-25 17:19:46 +0800

    Readme file model codes and main --ubinaroy

---
+++ README.md
@@ -0,0 +1,61 @@
+## USAGE
+`python3 main.py --type="train" --pre2train="True/False"` to train
+`python3 main.py --type="predict"` to predict
+`python3 main.py --type"generate" --sentence="INPUT HERE"` to generate a summary from the given sentences(1 or more)
+
+## FILE STRUCTURE
+```
+.
+├── checkpoint
+├── data
+├── evaluate.py
+├── generate.py
+├── lib
+│   ├── criterion.py
+│   ├── __init__.py
+│   ├── loss.py
+│   └── optimizer.py
+├── log
+├── main.py
+├── model
+│   ├── attention.py
+│   ├── decoder.py
+│   ├── embedding.py
+│   ├── encoder.py
+│   ├── generator.py
+│   ├── position_wise_feedforward.py
+│   ├── sublayer.py
+│   └── transformer.py
+├── mT5
+│   ├── arg.py
+│   ├── data.py
+│   ├── run_summarization_mt5.py
+│   ├── tools.py
+│   └── train_model_summarization.py
+├── parser.py
+├── prepare_data.py
+├── README.md
+├── run_summarization_mt5.sh
+├── summ_mt5_results
+├── train.py
+└── utils.py
+
+7 directories, 26 files
+```
+
+You can modify the project in parser.py
+
+## Pretrained mode
+You can use Hugging Face pretrained mode to process the target, but make sure there are enough GPU memory.
+run `bash run_summarization_mT5.sh` to make it, and modify in `mT5/arg.py` to fine tune.
+
+## REFERENCE
+[The Annotated Transformer](https://nlp.seas.harvard.edu/2018/04/03/attention.html)(一定要看这篇！！！可以直接看英文/上网搜翻译或者机翻)
+
+[Transformer --郑之杰](https://0809zheng.github.io/2020/04/25/transformer.html)
+
+[Transformer | PLM's](https://plmsmile.github.io/2018/08/29/48-attention-is-all-you-need/)
+
+[Transformer pytorch实现逐行详解](https://mdnice.com/writing/fc0b920d4ca84837a5712df1a46865d2)
+
+[从语言模型到Seq2Seq：Transformer如戏，全靠Mask](https://spaces.ac.cn/archives/6933)
---
+++ main.py
@@ -0,0 +1,126 @@
+import copy
+import os
+
+from parser import args
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from prepare_data import PrepareData, DataLoader
+from model.attention import MultiHeadedAttention
+from model.position_wise_feedforward import PositionwiseFeedForward
+from model.embedding import PositionalEncoding, Embeddings
+from model.transformer import Transformer
+from model.encoder import Encoder, EncoderLayer
+from model.decoder import Decoder, DecoderLayer
+from model.generator import Generator
+from lib.criterion import LabelSmoothing
+from lib.optimizer import NoamOpt
+from train import train
+from evaluate import evaluate
+from generate import generate
+
+
+def make_model(src_vocab, tgt_vocab, N = 6, d_model = 512, d_ff = 2048, h = 8, dropout = 0.1):
+    c = copy.deepcopy
+    attn = MultiHeadedAttention(h, d_model).to(args.device)
+    ff = PositionwiseFeedForward(d_model, d_ff, dropout).to(args.device)
+    position = PositionalEncoding(d_model, dropout).to(args.device)
+    model = Transformer(
+        Encoder(EncoderLayer(d_model, c(attn), c(ff), dropout).to(args.device), N).to(args.device),
+        Decoder(DecoderLayer(d_model, c(attn), c(attn), 
+                             c(ff), dropout).to(args.device), N).to(args.device),
+        nn.Sequential(Embeddings(d_model, src_vocab).to(args.device), c(position)),
+        nn.Sequential(Embeddings(d_model, tgt_vocab).to(args.device), c(position)),
+        Generator(d_model, tgt_vocab)).to(args.device)
+    
+    # This was important from their code. 
+    # Initialize parameters with Glorot / fan_avg.
+    for p in model.parameters():
+        if p.dim() > 1:
+            nn.init.xavier_uniform_(p)
+    return model.to(args.device)
+
+def main():
+    # 数据预处理
+    data = DataLoader()
+    print("Data loaded.\n")
+
+    args.src_vocab = len(data.src_word_dict)
+    args.tgt_vocab = len(data.tgt_word_dict)
+
+    print("Size:\nsrc_vocab %d" % args.src_vocab)
+    print("tgt_vocab %d\n" % args.tgt_vocab)
+
+    # 初始化模型
+    print("Modeling...")
+    model = make_model(
+                        args.src_vocab, 
+                        args.tgt_vocab, 
+                        args.layers, 
+                        args.d_model, 
+                        args.d_ff,
+                        args.h_num,
+                        args.dropout
+                    )
+    print("Model Ready.\n")
+
+
+
+   
+    if args.type == 'train':
+        # 训练
+        print(">>>>>>> Starting train\n")
+
+        if os.path.exists(args.save_model):
+          print("<<<<<< The pretrained model had already exsisted at %s.\n" % args.save_model)
+
+          if args.pre2train is not False:
+            print(">>>>>> Load the pretrained model and continue to train...\n")
+            model.load_state_dict(torch.load(args.save_model))
+            criterion = LabelSmoothing(args.tgt_vocab, padding_idx = 0, smoothing= 0.0)
+            optimizer = NoamOpt(args.d_model, 1, 2000, torch.optim.Adam(model.parameters(), lr=0, betas=(0.9,0.98), eps=1e-9))
+            train(data, model, criterion, optimizer)
+            print("<<<<<< Finished pre2train")
+
+          else:
+            print(">>>>>> Loading the pre-trained model at %s...\n" % args.save_model)
+            model.load_state_dict(torch.load(args.save_model))
+            print("<<<<<< Pre-trained model loaded.\n")
+
+        else:
+          criterion = LabelSmoothing(args.tgt_vocab, padding_idx = 0, smoothing= 0.0)
+          optimizer = NoamOpt(args.d_model, 1, 2000, torch.optim.Adam(model.parameters(), lr=0, betas=(0.9,0.98), eps=1e-9))
+
+          train(data, model, criterion, optimizer)
+
+        print("\n<<<<<<< Finished train\n")
+    elif args.type == "evaluate":
+        # 预测
+        # 先判断模型有没有训练好(前提)
+        if os.path.exists(args.save_model):
+            # 加载模型
+            model.load_state_dict(torch.load(args.save_model))
+            # 开始预测
+            print(">>>>>>> Starting evaluate")
+            evaluate(data, model, beam_size = args.beam_size)         
+            print("<<<<<<< Finished evaluate")
+        else:
+            print("Error: pleas train before evaluate")
+    
+    elif args.type == "generate":
+        if os.path.exists(args.save_model):
+            model.load_state_dict(torch.load(args.save_model))
+            print(">>>>>> Starting generate")
+            generate(data, model)
+            print("<<<<<< Finished generate")
+        else:
+            print("Error: please train before generate")
+
+
+    else:
+        print("Error: please select type within [train / evaluate]")
+
+if __name__ == "__main__":
+    main()
